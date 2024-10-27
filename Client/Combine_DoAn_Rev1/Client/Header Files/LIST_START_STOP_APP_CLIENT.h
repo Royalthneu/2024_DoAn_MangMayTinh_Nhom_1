b71407f4865c397023648b94fadc71dea5852699
23880092_Nguyễn Thế Trung <93558319+Royalthneu@users.noreply.github.com>
2024-10-27 10:43:19 +0700

    Update LIST_START_STOP_APP_CLIENT.h

--- Client/Combine_DoAn_Rev1/Client/Header Files/LIST_START_STOP_APP_CLIENT.h
+++ Client/Combine_DoAn_Rev1/Client/Header Files/LIST_START_STOP_APP_CLIENT.h
@@ -1,1 +1,9 @@
+#ifndef LIST_START_STOP_APP_CLIENT_H
+#define LIST_START_STOP_APP_CLIENT_H
 
+#include <winsock2.h>
+
+// Khai báo hàm cho menu quản lý ứng dụng
+void LIST_START_STOP_APP(SOCKET client_socket);
+
+#endif
