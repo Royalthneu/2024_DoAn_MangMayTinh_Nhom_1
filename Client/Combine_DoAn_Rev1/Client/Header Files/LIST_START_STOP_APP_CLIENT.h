7a77bd3ee9676d526576078cdb945461b1bf9453
23880092_Nguyễn Thế Trung <93558319+Royalthneu@users.noreply.github.com>
2024-10-27 10:47:54 +0700

    Delete Client/Combine_DoAn_Rev1 directory

--- Client/Combine_DoAn_Rev1/Client/Header Files/LIST_START_STOP_APP_CLIENT.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIST_START_STOP_APP_CLIENT_H
-#define LIST_START_STOP_APP_CLIENT_H
-
-#include <winsock2.h>
-
-// Khai báo hàm cho menu quản lý ứng dụng
-void LIST_START_STOP_APP(SOCKET client_socket);
-
-#endif
